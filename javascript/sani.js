a23ae4524488c5cce1bd9e379432cac6f1fcfc18
unknown <vhugodossantos33@gmail.com>
2024-09-06 20:10:01 -0300

    BETA

---
+++ javascript/sani.js
@@ -0,0 +1,12 @@
+function teste()
+{
+    var img = "/img/botijao-13kg-191x300.png"
+    var tema = document.getElementById("laranja")
+    tema.src = img;
+}
+function teste2()
+{
+    var img = "/img/gass-removebg-preview.png"
+    var tema = document.getElementById("laranja")
+    tema.src = img;
+}
