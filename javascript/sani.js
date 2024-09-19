87d81e8923b7a63e04dd920089abcbda26e4266f
Victor <162649665+Vitohs@users.noreply.github.com>
2024-09-19 18:46:09 -0300

    Add files via upload

--- javascript/sani.js
+++ javascript/sani.js
@@ -10,3 +10,28 @@ function teste2()
     var tema = document.getElementById("laranja")
     tema.src = img;
 }
+
+const senha = document.getElementById('senha')
+const confirma = document.getElementById('senha2')
+const escrita = document.getElementById('escrita')
+const form = document.getElementById('formee')
+
+function validar()
+{
+  if (senha.value !== confirma.value) {
+    escrita.textContent = 'As senhas não coincidem!'
+  } else {
+    escrita.textContent = ''
+  }
+}
+
+senha.onchange = validar()
+confirma.onkeyup = validar()
+
+form.onsubmit = function(event) 
+{
+    if (senha.value !== confirma.value) {
+      event.preventDefault();
+      escrita.textContent = 'As senhas não coincidem!';
+    }
+  }
